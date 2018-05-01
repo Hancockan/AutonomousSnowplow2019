8afbc6b845e1ee55b663d44e590b9811a7169bf0
kelleno96 <kellen.oconnor01@gmail.com>
2018-05-01 03:33:48 -0500

    Add files via upload

--- Autonomous Snowplow 2019/Autonomous Snowplow 2019/main.cpp
+++ Autonomous Snowplow 2019/Autonomous Snowplow 2019/main.cpp
@@ -3,6 +3,7 @@
 #include "lidar_handler.h"
 #include "SerialPort.h"
 #include "Arduino.h"
+#include "local_handler.h"
 
 using namespace std;
 char *port_name = "\\\\.\\COM9";
@@ -11,10 +12,12 @@ char *port_name = "\\\\.\\COM9";
 char incomingData[256] = "";
 
 int main() {
-	/*lidar_handler lidar;
+	lidar_handler lidar;
+	local_handler local;
 	std::thread lidar_thread(&lidar_handler::run, lidar);
-	lidar_thread.join();*/
-
+	std::thread local_thread(&local_handler::run, local);
+	//local_thread.join();
+	//lidar_thread.join();
 
 	Arduino a;
 	std::thread angle_thread(&Arduino::run, a);
@@ -25,9 +28,11 @@ int main() {
 	while (true)
 	{
 		cout << a.getAngle() << endl;
-		Sleep(10);
-		a.write(right);
+		Sleep(100);
+		a.write(left);
 	}
 	angle_thread.join();
+	//lidar_thread.join();
+	//local_thread.join();
 	return 0;
 }
