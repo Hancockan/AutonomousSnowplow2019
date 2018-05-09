049ea24d86c9c7a50fe21565aed11417f0c99262
kelleno96 <kellen.oconnor01@gmail.com>
2018-05-09 19:41:54 -0400

    Add files via upload

---
+++ AutonomousSnowplow2019/README.md
@@ -0,0 +1,2 @@
+# AutonomousSnowplow2019
+This repository contains code for the 2019 autonomous snowplow 
---
+++ AutonomousSnowplow2019/orientation_grabber.ino
@@ -0,0 +1,52 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+#define BNO055_SAMPLERATE_DELAY_MS (10)
+
+Adafruit_BNO055 bno = Adafruit_BNO055();
+
+void setup() {
+  Serial.begin(115200);
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  bno.setExtCrystalUse(true);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+}
+
+void loop() {
+  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  Serial.print("[");
+  Serial.print(360-euler.x());
+  Serial.println("]");
+  if(Serial.available())
+  {
+    char c = Serial.read();
+    if(c=='f')
+    {
+      digitalWrite(2, HIGH);
+      digitalWrite(3, LOW);
+      digitalWrite(4, LOW);
+    }
+    if(c=='l')
+    {
+      digitalWrite(2, LOW);
+      digitalWrite(3, HIGH);
+      digitalWrite(4, LOW);
+    }
+    if(c=='r')
+    {
+      digitalWrite(2, LOW);
+      digitalWrite(3, LOW);
+      digitalWrite(4, HIGH);
+    }
+  }
+  delay(BNO055_SAMPLERATE_DELAY_MS);
+}
