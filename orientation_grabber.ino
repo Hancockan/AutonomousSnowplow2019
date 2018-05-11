cbbbf454ca4fceed6acbc23f11c3a65b4aaccef4
Andrew Hancock <AndrewHancock0@Gmail.com>
2018-05-10 22:42:03 -0500

    arduino file from point to point movement

--- orientation_grabber.ino
+++ orientation_grabber.ino
@@ -2,13 +2,43 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
+#include <Servo.h>
+
+
+Servo left;
+Servo right;
 
 #define BNO055_SAMPLERATE_DELAY_MS (10)
 
 Adafruit_BNO055 bno = Adafruit_BNO055();
 
+void leftForward(Servo left){
+  left.write(180);
+}
+void leftBack(Servo left)
+{
+  left.write(0);
+}
+
+void rightForward(Servo right)
+{
+  right.write(180);
+}
+
+void rightBack(Servo right)
+{
+  right.write(0);
+}
+
+void stop(Servo right, Servo left)
+{
+  right.write(90);
+  left.write(90);
+}
 void setup() {
   Serial.begin(115200);
+  left.attach(9);
+  right.attach(10);
   if(!bno.begin())
   {
     /* There was a problem detecting the BNO055 ... check your connections */
@@ -34,18 +64,25 @@ void loop() {
       digitalWrite(2, HIGH);
       digitalWrite(3, LOW);
       digitalWrite(4, LOW);
+      rightForward(right);
+      leftForward(left);
+      
     }
     if(c=='l')
     {
       digitalWrite(2, LOW);
       digitalWrite(3, HIGH);
       digitalWrite(4, LOW);
+      rightForward(right);
+      leftBack(left);
     }
     if(c=='r')
     {
       digitalWrite(2, LOW);
       digitalWrite(3, LOW);
       digitalWrite(4, HIGH);
+      rightBack(right);
+      leftForward(left);
     }
   }
   delay(BNO055_SAMPLERATE_DELAY_MS);
